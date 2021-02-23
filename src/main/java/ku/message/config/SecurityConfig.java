f53c4f6afc1a19422ad8f2bf71cfd5798bef07a0
Mint <narisa.si@ku.th>
2021-02-23 23:04:19 +0700

    change bcrypt at least 13

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder(12);
+        return new BCryptPasswordEncoder(13);
     }
 
 
